f73934617659ab5fec94f865d56ac50d20c5de48
Kendal <kendalmfreeman@gmail.com>
2021-03-25 19:00:58 -0500

    Initial commit: MVC with Activities form

---
+++ UnsceneTravelApp/Controllers/ActivitiesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace UnsceneTravelApp.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
