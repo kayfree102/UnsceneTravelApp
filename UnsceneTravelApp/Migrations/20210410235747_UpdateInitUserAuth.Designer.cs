3d43e8aef35267e33d1bc1537dd27edd1b491783
Kendal <kendalmfreeman@gmail.com>
2021-04-10 20:22:21 -0500

    Added Authorization and updated migrations

--- UnsceneTravelApp/Migrations/20210410235747_UpdateInitUserAuth.Designer.cs
+++ UnsceneTravelApp/Migrations/20210410235747_UpdateInitUserAuth.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UnsceneTravelApp.Data;
 
 namespace UnsceneTravelApp.Migrations
 {
     [DbContext(typeof(ActivitiesDbContext))]
-    partial class ActivitiesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210410235747_UpdateInitUserAuth")]
+    partial class UpdateInitUserAuth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
